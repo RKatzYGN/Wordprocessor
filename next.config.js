b4769214dd8230db0d6bcd0ffd20a581009c0017
RKatzYGN <rabbimkatz@yeshivaofgreatneck.org>
2026-09-22 12:13:57 -0400

    Add webpack fallback configuration for client-side

--- next.config.js
+++ next.config.js
@@ -16,3 +16,19 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
